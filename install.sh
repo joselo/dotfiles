110d97887783f773ac0682ac95a7aead7986ceb7
joselo <joseloc@gmail.com>
2017-01-08 18:18:42 -0500

    add powerline theme

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ sudo pacman -S powerline powerline-fonts
 
 echo "Install oh-my-zsh" 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Install powerlevel9k"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9k
