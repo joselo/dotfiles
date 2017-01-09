e19bfb1aa7f52c7a9a2b9f2577100674396c6dc2
joselo <joseloc@gmail.com>
2017-01-08 19:41:31 -0500

    add ttf-fonts

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "Install powerline"
+echo "Installing powerline"
 sudo pacman -S powerline powerline-fonts
 
-echo "Install oh-my-zsh" 
+echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Install powerlevel9k"
+echo "Installing powerlevel9k"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/themes/powerlevel9k
+
+echo "Installing ttf-hackfont"
+sudo pacman -S ttf-hack
+
